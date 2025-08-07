cd6001f3db3ac1f894e8f970742af6eaf57431fc
Siddharth-Dhumal <dhumal.siddharth17@gmail.com>
2025-08-06 20:33:05 -0600

    Saving training feautres list to match the prediction data

--- src/feature_engineer.py
+++ src/feature_engineer.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from joblib import dump
 
 class FeatureEngineer:
 	def __init__(self):
@@ -12,11 +13,14 @@ class FeatureEngineer:
 		X = clean_df.drop("median_house_value", axis = 1)
 		X = pd.get_dummies(X)
 
+		self.feature_names = X.columns.tolist()
+
 		y = clean_df["median_house_value"]
 
 		scaled_X = self.scaler.fit_transform(X)
+		dump(self.scaler, "models/scaler.pkl")
 
 		X_train, X_test, y_train, y_test = train_test_split(
 			scaled_X, y, random_state = 42, test_size = 0.2, shuffle = True)
 
-		return X_train, X_test, y_train, y_test
+		return X_train, X_test, y_train, y_test, self.feature_names
